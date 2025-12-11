e54bfb99927e88a87c59cae871f179e416742a39
jojo <jojo@jojo.com>
2025-12-11 15:05:52 +0800

    update

---
+++ results-summary-component-main/vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+
+  // server: {
+  //   open: "./index_tailwind.html", // 启动时自动在浏览器中打开这个路径
+  // },
+});
